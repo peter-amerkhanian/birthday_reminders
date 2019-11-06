85c6429c0214b28c2baef8c778ebc1facbd17541
Peter Amerkhanian <peteamer@gmail.com>
2019-11-06 12:05:19 -0500

    fixed a bug in date comparison

--- send_texts.py
+++ send_texts.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 from text_messaging.parse_calendar import Birthday, get_birthdays
 from text_messaging.twilio_api_call import TextMessage
@@ -6,14 +6,14 @@ from text_messaging.twilio_api_call import TextMessage
 
 def main() -> None:
     message_sent: bool = False
-    today: str = datetime.today().strftime("%m-%d-%Y")
+    today: datetime = datetime.today()
     for birthday in get_birthdays(): # birthday: Birthday
-        if birthday.date.date() == today:
+        if birthday.date.date() == today.date():
             messenger: TextMessage = TextMessage(birthday.name, birthday.date)
             messenger.send_message()
             message_sent = True
     if not message_sent:
-        print("No birthdays today: {}".format(today))
+        print("No birthdays today: {}".format(today.strftime("%m-%d-%Y")))
 
 
 if __name__ == "__main__":
--- text_messaging/parse_calendar.py
+++ text_messaging/parse_calendar.py
@@ -13,7 +13,7 @@ def get_names() -> List[str]:
     with open('names.txt', 'r') as f_1:
         names_str: str = f_1.read()
         names: List[str] = names_str.split(',')
-        return names
+        return [name.strip().lower() for name in names]
 
 
 def get_birthdays() -> Iterator[Birthday]:
@@ -25,7 +25,7 @@ def get_birthdays() -> Iterator[Birthday]:
             if event.name == "VEVENT":
                 summary: str = str(event.get("SUMMARY"))
                 date_str: str = event.get("DTSTART").to_ical().decode('utf-8')
-                name: str = summary.replace("'s Birthday", "")
+                name: str = summary.replace("'s Birthday", "").strip().lower()
                 date: datetime = datetime.strptime(date_str, "%Y%m%d")
                 if name in get_names():
                     yield Birthday(name, date)
